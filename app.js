482f8ef608a57f8ac6405ab1e3081857c589dc2c
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2016-09-27 15:51:54 -0600

    fix multiple things playing

--- app.js
+++ app.js
@@ -25,3 +25,11 @@ function drawSongs(songList){
     songElement.innerHTML = template
 }
 
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
