f60fc9750b6f158ddb9eff5743a0e5989bff4641
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2016-10-05 15:32:44 -0600

    Added personal list functionality

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-//Do Not Modify the getMusic function
-function getMusic(){
-   var artist = document.getElementById('artist').value;
-  itunes.getMusicByArtist(artist).then(drawSongs);
-}
+Controller()
 
-function drawSongs(songList){
-  console.log(songList);
-  var template = `<h3>Song Results Found: ${songList.length}</h3>`
-  var songElement = document.getElementById('allsongs')
+function Controller() {
+  var dataStore = new getMyMusic()
+
+  function drawSongs(songList, control) {
+    var songElement = $('#allsongs')
+    var template = `<h3>Song Results Found: ${songList.length}</h3>`
+    var button = ''
     for (var i = 0; i < songList.length; i++) {
       var song = songList[i]
+      if (control == 'remove') {
+        button = `<button class="remove"><i id="${song.id}" class="fa fa-minus-square" aria-hidden="true"></i></span></button>
+                  <button class="prom"><i id="${song.id}" class="fa fa-chevron-up" aria-hidden="true"></i></button>
+                  <button class="dem"><i id="${song.id}" class="fa fa-chevron-down" aria-hidden="true"></i></button>`
+      } else {
+        button = `<button class="add"><i id="${song.id}" class="fa fa-plus-square" aria-hidden="true"></i></button>`
+      }
       template += `
       <div class="song">
         <div class="img-container">
@@ -18,18 +24,46 @@ function drawSongs(songList){
         <div class="text-container">
           <p><b>${song.title}</b> &mdash; <em>${song.artist}</em> — Album: '${song.collection}' &mdash; $${song.price}</p>
           <audio controls preload="none">
-          <source src="${song.preview}" type="audio/mp4" /></audio>
+          <source src="${song.preview}" type="audio/mp4" /></audio>` + button + `
         </div>
       </div>`
     }
-    songElement.innerHTML = template
-}
-
-document.addEventListener('play', function(e){
+    songElement.html(template)
+  }
+  $('#allsongs').on('click', 'button.add', function (event) {
+    dataStore.addTrack(event.target.id)
+    drawSongs(dataStore.getCurrentSongList(), 'add')
+  })
+  $('#allsongs').on('click', 'button.remove', function (event) {
+    dataStore.removeTrack(event.target.id)
+    drawSongs(dataStore.getMyMusic(), 'remove')
+  })
+  $('#allsongs').on('click', 'button.prom', function (event) {
+    dataStore.promoteTrack(event.target.id)
+    drawSongs(dataStore.getMyMusic(), 'remove')
+  })
+  $('#allsongs').on('click', 'button.dem', function (event) {
+    dataStore.demoteTrack(event.target.id)
+    drawSongs(dataStore.getMyMusic(), 'remove')
+  })
+  $('#art-search').on('submit', function getMusic(event) {
+    event.preventDefault();
+    var form = event.target;
+    var artist = form.artist.value
+    dataStore.getMusicByArtist(artist).then(drawSongs, 'add');
+  })
+  $('#art-search').on('click', 'button.my-music', function (event) {
+    drawSongs(dataStore.getMyMusic(), 'remove')
+  })
+  document.addEventListener('play', function (e) {
     var audios = document.getElementsByTagName('audio');
-    for(var i = 0, len = audios.length; i < len;i++){
-        if(audios[i] != e.target){
-            audios[i].pause();
-        }
+    for (var i = 0, len = audios.length; i < len; i++) {
+      if (audios[i] != e.target) {
+        audios[i].pause();
+      }
     }
-}, true);
+  }, true);
+  this.emptyMusic = function () {
+    dataStore.clearData()
+  }
+}
